dfe49829c6475b6af830580e4ccf9d946eac15d2
Huy Ly <HuyLy@Super.local>
2018-06-14 18:21:21 +0700

    Migrate auneor_etiquette_J8160 from v8 to v10

---
+++ auneor_etiquette_J8160/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Auneor Etiquette K8160',
+    'version': '10.0.1.0.0',
+    'category': 'Uncategorized',
+    'author': "Auneor Conseil",
+    'website': 'http://www.auneor-conseil.fr',
+    "license": "AGPL-3",
+    'depends': [
+        'base',
+        'report',
+    ],
+    'data': [
+        # Data
+        'data/report_paperformat_data.xml',
+        # Views
+        'views/res_partner_report.xml',
+        'views/res_partner_report_address.xml',
+    ]
+}
